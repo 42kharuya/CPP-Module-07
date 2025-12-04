dd031ff061fd231dae021936054f3cb6bbe93797
42kharuya <haruya.0411.k@gmail.com>
2025-12-04 22:47:29 +0900

    Fix array initialization and add value checks in main.cpp; remove unused test.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,16 +3,65 @@
 #include <string>
 #include <stdlib.h>
 
-#define MAX_VAL 10
+#define MAX_VAL 5
 
 int main() {
+
+	{
+		std::cout << "\n////// Default Test //////" << std::endl;
+
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;
+	}
 
 	{
 		std::cout << "\n////// Normal Int, Address Check //////" << std::endl;
 
 		Array<int> numbers(MAX_VAL);
 		int* mirror = new int[MAX_VAL];
-		srand(time(NULL));
 		for (int i = 0; i < MAX_VAL; ++i) {
 			const int value = rand();
 			numbers[i] = value;
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-	std::string *a = new std::string[0];
-
-	std::cout << a[0] << std::endl;
-	delete[] a;
-}
